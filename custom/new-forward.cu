07faf6c44f5e1cdc06e600b9a681d0be7fa4e11c
crides <zhuhaoqing@live.cn>
2020-11-17 20:38:59 -0600

    Base

--- custom/new-forward.cu
+++ custom/new-forward.cu
@@ -17,11 +17,8 @@
 #define k4d(k, i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 #define for_in(v, max) for (int v = 0; v < max; v++)
 
-#define INT_FACTOR 4096
-#define INT_TYPE int
-
-#define TILE_WIDTH 32
-__global__ void conv_forward_kernel(float *y, INT_TYPE *temp, const INT_TYPE *k, const int B, const int M, const int C, const int H, const int W, const int K)
+#define TILE_WIDTH 16
+__global__ void conv_forward_kernel(float *y, float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
@@ -38,44 +35,15 @@ __global__ void conv_forward_kernel(float *y, INT_TYPE *temp, const INT_TYPE *k,
     const int m = blockIdx.y;
 
     if (h < H_out && w < W_out) {
-        INT_TYPE acc = 0;
+        float acc = 0;
         for_in(c, C) {
             for_in(p, K) {
                 for_in(q, K) {
-                    acc += x4d(temp, b, c, h + p, w + q) * k4d(k, m, c, p, q) / INT_FACTOR;
+                    acc += x4d(x, b, c, h + p, w + q) * k4d(k, m, c, p, q);
                 }
             }
         }
-        y4d(y, b, m, h, w) = __int2float_rd(acc) / INT_FACTOR;
-    }
-}
-
-__global__ void input_to_fixed_point(const float *input, INT_TYPE *output, const int B, const int M, const int C, const int H, const int W, const int K) {
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    const int w_grid = (int) ceil(((float) W_out) / TILE_WIDTH);
-
-	// Recover the blockidx in x and y dim using Z
-	const int H_bi = blockIdx.z / w_grid;
-	const int W_bi = blockIdx.z % w_grid;
-	
-	// h and w refers to both the output and input index(some of the threads will be turned off during output)
-    const int h = H_bi * TILE_WIDTH + threadIdx.y;
-    const int w = W_bi * TILE_WIDTH + threadIdx.x;
-    const int b = blockIdx.x;
-
-    if (h < H && w < W) {
-        for_in(c, C) {
-            float in = x4d(input, b, c, h, w);
-            x4d(output, b, c, h, w) = __float2int_rd(in * INT_FACTOR);
-        }
-    }
-}
-
-__global__ void kernel_to_fixed_point(const float *input, INT_TYPE *output, const int B, const int M, const int C, const int H, const int W, const int K) {
-    int m = blockIdx.x, c = blockIdx.y, p = threadIdx.x, q = threadIdx.y;
-    if (m < M && c < C && p < K && q < K) {
-        k4d(output, m, c, p, q) = __float2int_rd(k4d(input, m, c, p, q) * INT_FACTOR);
+        y4d(y, b, m, h, w) = acc;
     }
 }
 
@@ -83,7 +51,7 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
 {
     // Declare relevant device pointers
     float *dev_x, *dev_y, *dev_k;
-    INT_TYPE *temp, *dev_k_16;
+    float *dev_k_16;
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
@@ -92,33 +60,24 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
     size_t y_size = B * M * H_out * W_out * sizeof(float);
     size_t k_len = M * C * K * K;
     cuda_check(cudaMalloc(&dev_x, x_len * sizeof(float)));
-    cuda_check(cudaMalloc(&temp, x_len * sizeof(INT_TYPE)));
     cuda_check(cudaMalloc(&dev_y, y_size));
     cuda_check(cudaMalloc(&dev_k, k_len * sizeof(float)));
-    cuda_check(cudaMalloc(&dev_k_16, k_len * sizeof(INT_TYPE)));
+    cuda_check(cudaMalloc(&dev_k_16, k_len * sizeof(float)));
     cuda_check(cudaMemcpy(dev_k, host_k, k_len * sizeof(float), cudaMemcpyHostToDevice));
-
-    dim3 dim_grid_kernel(M, C, 1);
-    dim3 dim_block_kernel(K, K, 1);
-    kernel_to_fixed_point<<<dim_grid_kernel, dim_block_kernel>>>(dev_k, dev_k_16, B, M, C, H, W, K);
 
     const int H_grid = ceil((float) H_out / TILE_WIDTH);
     const int W_grid = ceil((float) W_out / TILE_WIDTH);
     const int Z = H_grid * W_grid;
     dim3 dim_grid(B, M, Z);
     dim3 dim_block(TILE_WIDTH, TILE_WIDTH, 1);
-    const int Z_input = ceil((float) H / TILE_WIDTH) * ceil((float) W / TILE_WIDTH);
-    dim3 dim_grid_conv_input(B, M, Z_input);
 
     cuda_check(cudaMemcpy(dev_x, host_x, x_len * sizeof(float), cudaMemcpyHostToDevice));
-    input_to_fixed_point<<<dim_grid_conv_input, dim_block>>>(dev_x, temp, B, M, C, H, W, K);
-    conv_forward_kernel<<<dim_grid, dim_block>>>(dev_y, temp, dev_k_16, B, M, C, H, W, K);
+    conv_forward_kernel<<<dim_grid, dim_block>>>(dev_y, dev_x, dev_k, B, M, C, H, W, K);
     cuda_check(cudaMemcpy(host_y, dev_y, y_size, cudaMemcpyDeviceToHost));
 
     // Free device memory
     cuda_check(cudaFree(dev_x));
     cuda_check(cudaFree(dev_k_16));
-    cuda_check(cudaFree(temp));
     cuda_check(cudaFree(dev_y));
     cuda_check(cudaFree(dev_k));
 }
